4f6f79ff1539d07000fe147c73dcf9322915983f
mohammad ashour <moh.ash96@gmail.com>
2021-03-02 11:40:43 +0200

    added remove function and modified counter

--- js/cart.js
+++ js/cart.js
@@ -1,99 +1,128 @@
-'use strict';
+"use strict";
 const CartI = function (name, price, type, path) {
-    this.name = name;
-    this.price = price;
-    this.type = type;
-    this.path = path;
-    CartI.allProducts.push(this);
-}
+  this.name = name;
+  this.price = price;
+  this.type = type;
+  this.path = path;
+  CartI.allProducts.push(this);
+};
 CartI.allProducts = [];
-new CartI('lower', 15, 'lowers', '../assets/accessories/accssories1.jpg');
-new CartI('lower', 15, 'lowers', '../assets/accessories/accssories2.jpg');
-new CartI('ower', 12, 'owers', '../assets/accessories/accssories3.jpg');
-let button = document.getElementById('test');
-button.addEventListener('click', submitter);
+new CartI("flower1", 15, "lowers", "../assets/accessories/accssories1.jpg");
+new CartI("flower2", 15, "lowers", "../assets/accessories/accssories2.jpg");
+new CartI("flower3", 12, "owers", "../assets/accessories/accssories3.jpg");
+let button = document.getElementById("test");
+button.addEventListener("click", submitter);
 function submitter(event) {
-    event.preventDefault();
-    localStorage.setItem('added', JSON.stringify(CartI.allProducts));
+  //   event.preventDefault();
+  localStorage.setItem("added", JSON.stringify(CartI.allProducts));
 }
 
+////////////////////////////////////////OUR WORK STARTS HERE!////////////////////////////////////////////////////
 
+let cartCont = document.getElementById("tableResult");
+cartCont.addEventListener("click", removeItem);
 
-
-// here loadcart starts
 let cartItems;
+
 function loadCart() {
-    for (let i = 0; i < localStorage.length; i++) {
-        cartItems = JSON.parse(localStorage.getItem(`added`)) || [];
-    }
-
+  for (let i = 0; i < localStorage.length; i++) {
+    cartItems = JSON.parse(localStorage.getItem(`added`)) || [];
+  }
 }
-loadCart();
-console.log(cartItems);
-let cartCont = document.getElementById('tableResult');
 
-for (let i = 0; i < cartItems.length; i++) {
+loadCart(); // This one gets the local storage contents
 
-    let tableRow = document.createElement('tr');
+addCart(); // this will render cart
+itemCount(); // items counter
+
+function addCart() {
+  for (let i = 0; i < cartItems.length; i++) {
+    let tableRow = document.createElement("tr");
     cartCont.appendChild(tableRow);
 
-    let naming = document.createElement('td');
+    let naming = document.createElement("td");
     tableRow.appendChild(naming);
     naming.textContent = cartItems[i].name;
-    console.log(cartItems[i].name)
-    let itemImg = document.createElement('img');
-    itemImg.setAttribute('src', `${cartItems[i].path}`);
+
+    let itemImg = document.createElement("img");
+    itemImg.setAttribute("src", `${cartItems[i].path}`);
     tableRow.appendChild(itemImg);
-    let quantity = document.createElement('td');
+
+    let quantity = document.createElement("td");
     tableRow.appendChild(quantity);
-    let inputQuantity = document.createElement('input');
-    inputQuantity.setAttribute('type', 'number');
-    inputQuantity.setAttribute('value', '1');
-    inputQuantity.setAttribute('min', '1');
-    inputQuantity.setAttribute('name', 'quantity');
-    inputQuantity.setAttribute('id', 'quan');
+    let inputQuantity = document.createElement("input");
+    inputQuantity.setAttribute("type", "number");
+    inputQuantity.setAttribute("value", "1");
+    inputQuantity.setAttribute("min", "1");
+    inputQuantity.setAttribute("name", "quantity");
+    inputQuantity.setAttribute("id", "quan");
     quantity.appendChild(inputQuantity);
-    console.log(inputQuantity);
-    let pricing = document.createElement('td');
+
+    let pricing = document.createElement("td");
     tableRow.appendChild(pricing);
     pricing.textContent = cartItems[i].price;
-    let remove = document.createElement('td');
+
+    let remove = document.createElement("td");
     tableRow.appendChild(remove);
-    remove.setAttribute('id', i);
-    remove.textContent = 'x';
+    remove.setAttribute("id", i);
+    remove.textContent = "x";
+  }
 }
 
-console.log(localStorage);
+// popup message code
 
+function popUp(params) {
+  let modal = document.getElementById("myModal");
 
-let itemCount = document.getElementById('totalItems').textContent = cartItems.length;
+  let btn = document.getElementById("myBtn");
 
+  let span = document.getElementsByClassName("close")[0];
 
-// popup message code 
-
-
-let modal = document.getElementById("myModal");
-
-
-let btn = document.getElementById("myBtn");
-
-let span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function () {
+  btn.onclick = function () {
     modal.style.display = "block";
     localStorage.clear();
     loadCart();
     setTimeout(function () {
-        location.reload();
+      location.reload();
     }, 3000);
-}
+  };
 
-span.onclick = function () {
+  span.onclick = function () {
     modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 }
 
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+popUp();
+
+function removeOne(item) {
+  cartItems.splice(item, 1);
 }
+
+function removeItem(event) {
+  const item = event.target.id;
+  if (item !== "") {
+    removeOne(item);
+    clearCart();
+    addCart();
+    loadCart();
+  }
+}
+
+function clearCart() {
+  document.getElementById("tableResult").textContent = "";
+  localStorage.clear();
+  itemCount();
+}
+
+function itemCount() {
+  let itemCount = (document.getElementById("totalItems").textContent =
+    cartItems.length);
+}
+console.log(cartItems);
+loadCart();
