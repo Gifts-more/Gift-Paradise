d425e45881d4a66fc4929f5585dfafd1aa613354
Ahmed Karasneh <karasneh94@gmail.com>
2021-03-02 09:03:18 +0200

    from karasneh

--- js/app.js
+++ js/app.js
@@ -2,129 +2,197 @@
 /* eslint-disable indent */
 'use strict';
 
-const Product = function (name, price, type, path, quantity) {
-  this.name = name;
-  this.price = price;
-  this.type = type;
-  this.path = path;
-  this.changeQuantity = function (qua) {
+
+let items = document.getElementById('items');
+let cartItems = [];
+let firstRun = true;
+
+const Product = function (name, price, type, path) {
+    this.name = name;
+    this.price = price;
+    this.type = type;
+    this.path = path;
     this.quantity = 0;
-  };
-  Product.allProducts.push(this);
+    Product.allProducts.push(this);
 };
 Product.allProducts = [];
 
-const Cart = function (items) {
-  this.items = items;
-};
 
-let items = document.getElementById('items');
-let cat = function (category) {
-  for (let i = 0; i < Product.allProducts.length; i++) {
-    if (Product.allProducts[i].type === category) {
-      let item = document.createElement('div');
-      item.setAttribute('class', 'item');
-      items.appendChild(item);
-      let img = document.createElement('img');
-      img.setAttribute('class', 'itemImg');
-      img.setAttribute('src', Product.allProducts[i].path);
-      item.appendChild(img);
-      let info = document.createElement('div');
-      item.appendChild(info);
-      let leftInfo = document.createElement('div');
-      leftInfo.setAttribute('class', 'leftInfo');
-      info.appendChild(leftInfo);
-      let itemName = document.createElement('h3');
-      leftInfo.appendChild(itemName);
-      itemName.textContent = Product.allProducts[i].name;
-      let itemPrice = document.createElement('h4');
-      leftInfo.appendChild(itemPrice);
-      itemPrice.textContent = Product.allProducts[i].price + ' JD';
-      let itemQ = document.createElement('input');
-      itemQ.setAttribute('type', 'number');
-      itemQ.setAttribute('id', `quantity${i}`);
-      itemQ.setAttribute('min', '1');
-      itemQ.setAttribute('value', '1');
-      itemQ.setAttribute('name', 'quantity');
-      leftInfo.appendChild(itemQ);
 
-      let rightInfo = document.createElement('div');
-      rightInfo.setAttribute('class', 'rightInfo');
-      info.appendChild(rightInfo);
 
-      let addBtn = document.createElement('button');
-      addBtn.setAttribute('id', i);
-      rightInfo.appendChild(addBtn);
-      addBtn.textContent = 'Add';
+
+
+
+function renderItems(type) {
+
+    switch (type) {
+        case 'flowers':
+            generate('flowers');
+
+            break;
+        case 'chocolate':
+            generate('chocolate');
+            break;
+        case 'candy':
+            generate('candy');
+            break;
+        case 'gift-boxes':
+            generate('gift-boxes');
+            break;
+        case 'accessories':
+            generate('accessories');
+            break;
+        case 'balloons':
+            generate('balloons');
+            break;
+
+        default:
+
+            generate('all');
+            console.log('default switch');
+
+            break;
     }
-  }
-};
-
-function generateItems(type) {
-  if (type === 'flowers') {
-    new Product('candy1', 10, 'flowers', 'assets/candy1.jpg', 0);
-    new Product('candy2', 120, 'flowers', 'assets/candy2.jpg', 0);
-  }
-  if (type === 'chocolate') {
-    new Product('candy3', 30, 'chocolate', 'assets/candy3.jpg', 0);
-  }
-  if (type === 'candy') {
-
-  }
-
-  if (type === 'gift-boxes') {
-
-  }
-  if (type === 'accessories') {
-
-  }
-  if (type === 'baloons') {
-
-  }
-
-  
-
-  switch (type) {
-    case 'flowers':
-      cat('flowers');
-
-      break;
-    case 'chocolate':
-      cat('chocolate');
-      break;
-    case 'candy':
-      cat('candy');
-      break;
-    case 'gift-boxes':
-      cat('gift-boxes');
-      break;
-    case 'accessories':
-      cat('accessories');
-      break;
-    case 'baloons':
-      cat('baloons');
-      break;
-
-    default:
-      break;
-  }
 }
 
-function submitter(event) {
-  // event.preventDefault();
 
-  for (let i = 0; i < Product.allProducts.length; i++) {
-    if (event.target.id == i) {
-      let getQuantity = document.getElementById(`quantity${i}`).value;
-      Product.allProducts[i].changeQuantity(getQuantity);
-      localStorage.setItem(`added${i}`, JSON.stringify(Product.allProducts[i]));
+
+function generate(type) {
+
+    for (let i = 0; i < Product.allProducts.length; i++) {
+
+        if (Product.allProducts[i].type === type) {
+
+            let item = document.createElement('div');
+            item.setAttribute('class', 'item');
+            items.appendChild(item);
+            let img = document.createElement('img');
+            img.setAttribute('class', 'itemImg');
+            img.setAttribute('src', Product.allProducts[i].path);
+            item.appendChild(img);
+            let info = document.createElement('div');
+            item.appendChild(info);
+            let leftInfo = document.createElement('div');
+            leftInfo.setAttribute('class', 'leftInfo');
+            info.appendChild(leftInfo);
+            let itemName = document.createElement('h3');
+            leftInfo.appendChild(itemName);
+            itemName.textContent = Product.allProducts[i].name;
+            let itemPrice = document.createElement('h4');
+            leftInfo.appendChild(itemPrice);
+            itemPrice.textContent = Product.allProducts[i].price + ' JD\'s';
+
+            let rightInfo = document.createElement('div');
+            rightInfo.setAttribute('class', 'rightInfo');
+            info.appendChild(rightInfo);
+
+            let addBtn = document.createElement('button');
+            addBtn.setAttribute('id', i);
+            rightInfo.appendChild(addBtn);
+            addBtn.textContent = 'Add to Cart';
+        } else if (type === 'all') {
+
+            let item = document.createElement('div');
+            item.setAttribute('class', 'item');
+            items.appendChild(item);
+            let img = document.createElement('img');
+            img.setAttribute('class', 'itemImg');
+            img.setAttribute('src', Product.allProducts[i].path);
+            item.appendChild(img);
+            let info = document.createElement('div');
+            item.appendChild(info);
+            let leftInfo = document.createElement('div');
+            leftInfo.setAttribute('class', 'leftInfo');
+            info.appendChild(leftInfo);
+            let itemName = document.createElement('h3');
+            leftInfo.appendChild(itemName);
+            itemName.textContent = Product.allProducts[i].name;
+            let itemPrice = document.createElement('h4');
+            leftInfo.appendChild(itemPrice);
+            itemPrice.textContent = Product.allProducts[i].price + ' JD\'s';
+
+            let rightInfo = document.createElement('div');
+            rightInfo.setAttribute('class', 'rightInfo');
+            info.appendChild(rightInfo);
+
+            let addBtn = document.createElement('button');
+            addBtn.setAttribute('id', i);
+            rightInfo.appendChild(addBtn);
+            addBtn.textContent = 'Add to Cart';
+
+
+        }
     }
-  }
-}
 
-generateItems('flowers');
+
+} //end of generate function
+
+
+
+
+new Product('candy1', 10, 'candy', 'assets/candies/candy1.jpg');
+new Product('flower', 120, 'flowers', 'assets/flowers/lavander flower.jpg');
+new Product('balloons', 120, 'balloons', 'assets/balloons/balloons.jpg');
+new Product('balloons', 120, 'balloons', 'assets/balloons/balloons.jpg');
+new Product('balloons', 120, 'balloons', 'assets/balloons/balloons.jpg');
+
+renderItems('all');
+
 
 for (let i = 0; i < Product.allProducts.length; i++) {
-  let addbutton = document.getElementById(i);
-  addbutton.addEventListener('click', submitter);
+    let addbutton = document.getElementById(i);
+    addbutton.addEventListener('click', submitter);
 }
+
+
+
+function submitter(event) {
+    event.preventDefault();
+    console.log('inside submitter');
+    console.log(event.target.id);
+
+    if (firstRun) {
+
+        let storedCart = localStorage.getItem('cartItems');
+
+        if (storedCart) {
+
+            cartItems = JSON.parse(storedCart);
+
+        }
+        firstRun = false;
+        localStorage.removeItem('cartItems');
+        sessionStorage.removeItem('cartItems');
+
+    }
+
+
+
+
+
+
+    if (cartItems.includes(Product.allProducts[event.target.id])) {
+
+        console.log('increase quantity before refreshing');
+        let index = cartItems.indexOf(Product.allProducts[event.target.id]);
+        cartItems[index].quantity++;
+
+    } else {
+
+        console.log('adding a new item');
+        Product.allProducts[event.target.id].quantity++;
+        cartItems.push(Product.allProducts[event.target.id]);
+
+    }
+
+
+    let data = JSON.stringify(cartItems);
+    localStorage.setItem('cartItems', data);
+
+
+
+}
+
+
+
+
